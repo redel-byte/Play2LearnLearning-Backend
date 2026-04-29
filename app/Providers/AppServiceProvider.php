6c6df1c776864a03651c80114bb373cdcd8b1118
redel-byte <alhabibridouane@gmail.com>
2026-04-29 10:10:37 +0100

    refactor: removing unessasry logic function in the appserviceprovvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot(): void {}
 }
